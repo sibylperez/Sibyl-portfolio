d712c28bfc62693719b770b60a7b60b22682d673
Sibyl Pérez <sibylperezl@gmail.com>
2021-09-01 17:10:49 -0300

    Services section

---
+++ src/components/SectionHomeServices/ServicesSection.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { MdDesktopMac, MdCode, MdPhonelinkSetup } from 'react-icons/md';
+import styled from 'styled-components';
+import Title from '../utils/Title';
+import ServicesIcons from './ServicesIcons';
+
+const ServicesItemsStyles = styled.div`
+  padding: 10rem 0;
+  .services__allItems {
+    display: flex;
+    gap: 10rem;
+    justify-content: space-between;
+    margin-top: 5rem;
+  }
+  @media only screen and (max-width: 768px) {
+    .services__allItems {
+      flex-direction: column;
+      max-width: 350px;
+      margin: 0 auto;
+      margin-top: 5rem;
+      gap: 5rem;
+    }
+  }
+`;
+
+export default function ServicesSection() {
+  return (
+    <ServicesItemsStyles>
+      <div className="container">
+        <Title subheading="What I will do for you" heading="Services" />
+        <div className="services__allItems">
+          <ServicesIcons
+            icon={<MdDesktopMac />}
+            title="web design"
+            desc="Design ui/ux for the website to get a unique look."
+          />
+          <ServicesIcons
+            icon={<MdCode />}
+            title="web dev"
+            desc="Develop the websites. Programming languages: REACT, REDUX, NODEJS, POSTGRESQL"
+          />
+          <ServicesIcons
+            icon={<MdPhonelinkSetup />}
+            title="app Dev"
+            desc="Loading.... Studies in progress. "
+          />
+        </div>
+      </div>
+    </ServicesItemsStyles>
+  );
+}
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Landing from '../../components/Landing/LandingPhoto';
 import AboutSection from '../../components/SectionHomeAbout/AboutSection';
+import ServicesSection from '../../components/SectionHomeServices/ServicesSection';
 
 export default function Home() {
   return (
     <div>
       <Landing />
       <AboutSection />
+      <ServicesSection />
     </div>
   );
 }
