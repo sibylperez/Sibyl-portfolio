6093808255370a636f729c18f2e98528a8f9ec87
Sibyl Pérez <sibylperezl@gmail.com>
2021-09-01 11:41:29 -0300

    add(New Section) modified Landing css

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Landing from '../../components/Landing/LandingPhoto';
+import AboutSection from '../../components/SectionHomeAbout/AboutSection';
 
 export default function Home() {
   return (
     <div>
       <Landing />
+      <AboutSection />
     </div>
   );
 }
