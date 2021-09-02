fa965388c4db9be930ca4ccbec036e727df44b59
Sibyl Pérez <sibylperezl@gmail.com>
2021-09-02 19:13:27 -0300

    Footer Done

--- src/components/Footer/FooterContact.js
+++ src/components/Footer/FooterContact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TextLanding from '../Landing/TextLanding';
+import FooterCol from './FooterCol';
 import './FooterContact.css';
 
 export default function FooterContact() {
@@ -12,6 +13,76 @@ export default function FooterContact() {
             Full Stack Developer and designer. Prepare to create a true-to-brand
             appearance for a client. Committed to developing, organizing,
             managing and executing your projects.
+          </TextLanding>
+        </div>
+        <div className="footerCol2">
+          <FooterCol
+            heading="Important Links"
+            links={[
+              {
+                title: 'Home',
+                path: '/',
+                type: 'Link',
+              },
+              {
+                type: 'Link',
+                title: 'About',
+                path: '/about',
+              },
+              {
+                type: 'Link',
+                title: 'Projects',
+                path: '/projects',
+              },
+              {
+                type: 'Link',
+                title: 'Contact',
+                path: '/contact',
+              },
+            ]}
+          />
+        </div>
+        <div className="footerCol3">
+          <FooterCol
+            heading="Social Links"
+            links={[
+              {
+                title: 'LinkedIn',
+                path: 'https://www.linkedin.com/in/sibyl-perez/',
+              },
+              {
+                title: 'Github',
+                path: 'https://github.com/sibylperez',
+              },
+              {
+                title: 'Twitter',
+                path: 'https://twitter.com/Lady_Sweet_S',
+              },
+            ]}
+          />
+        </div>
+        <div className="footerCol4">
+          <FooterCol
+            heading="Contact Info"
+            links={[
+              {
+                title: 'sibylperezl@gmail.com',
+                path: 'mailto:sibylperezl@gmail.com',
+              },
+              {
+                title: 'Buenos Aires, Argentina',
+                path:
+                  'https://www.google.com/maps/@-34.6144314,-58.4059233,14z',
+              },
+            ]}
+          />
+        </div>
+      </div>
+      <div className="copyright">
+        <div className="container">
+          <TextLanding>
+            {' '}
+            © 2021 copyright - Sibyl María | Pérez Rueda{' '}
           </TextLanding>
         </div>
       </div>
