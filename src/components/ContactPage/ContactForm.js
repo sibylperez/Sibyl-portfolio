36ef861716053b42cdef73ac00f50ee228ce2bff
Sibyl Pérez <sibylperezl@gmail.com>
2021-09-04 19:56:04 -0300

    fix(error)2

--- src/components/ContactPage/ContactForm.js
+++ src/components/ContactPage/ContactForm.js
@@ -21,7 +21,7 @@ export default function ContactForm() {
             <input
               type="hidden"
               name="redirect"
-              value="sibyl-portfolio.netlify.app"
+              value="https://sibyl-portfolio.netlify.app"
             />
 
             <div className="mb-6">
